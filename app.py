ddc951ed7db78ac2acdd0d643ed6f03b142a5f6f
Luiz Humphries <luizgustavo.frota@outlook.com>
2024-01-24 13:47:28 -0300

    Food Model updated, Start of Creating Food Function/Route

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request
 from database import db
+from models.food import Food
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:admin@127.0.0.1:3306/daily-diet-crud"
 db.init_app(app)
+
+
+@app.route('/food', methods=['POST'])
+def create_food():
+    request_data = request.json    
+    pass
+
+
+
+
+
 
 
 if "__name__" == "__main__":
--- models/food.py
+++ models/food.py
@@ -1,14 +1,14 @@
 import datetime
-from typing import List
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped
 from database import db
 
 class Food(db.Model):
     """Default Table Model For Food insertion"""
-    id: Mapped[int] = mapped_column(primary_key=True) #unique
-    name: Mapped[str] #Non Nullable
-    description: Mapped[str] #Non Nullable
-    time: Mapped[datetime.datetime] #Non Nullable
-    diet: Mapped[bool] #Non Nullable
+    id: Mapped[int] = db.Column(db.Integer, primary_key=True) #unique
+    name: Mapped[str] = db.Column(db.String(80), nullable=False) #Non Nullable
+    description: Mapped[str] = db.Column(db.String(120), nullable=False) #Non Nullable
+    calories: Mapped[int] = db.Column(db.Float, nullable=False) #Non Nullable
+    diet: Mapped[bool] = db.Column(db.Boolean,default=False, nullable=False) #Non Nullable
+    time: Mapped[datetime.datetime] = db.Column(db.DateTime(timezone=True), nullable=False) #Non Nullable
 
     
